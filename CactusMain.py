242286494b2f4871bae6470ed52e9658cafef500
Karsten Wolf <karstenwo@web.de>
2012-04-07 23:52:59 +0200

    CactusMain added. I wanted to have a mostly empty main file.

---
+++ CactusMain.py
@@ -0,0 +1,32 @@
+
+# -*- coding: utf-8 -*-
+
+
+import PyObjCTools
+import PyObjCTools.NibClassBuilder
+extractClasses = PyObjCTools.NibClassBuilder.extractClasses
+AutoBaseClass = PyObjCTools.NibClassBuilder.AutoBaseClass
+
+import PyObjCTools.AppHelper
+AppHelper = PyObjCTools.AppHelper
+
+
+extractClasses("MainMenu")
+extractClasses("OpenURL")
+
+extractClasses("TableEditor")
+extractClasses("OutlineEditor")
+extractClasses("NodeEditor")
+
+
+import CactusOPMLDocument
+import CactusTableDocument
+
+
+import Outline
+import Cactus
+
+
+if __name__ == "__main__":
+    from PyObjCTools import AppHelper
+    AppHelper.runEventLoop()
