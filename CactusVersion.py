0387262a8d8ddf10fafc64cf20ea865ef3145f08
Karsten Wolf <karstenwo@web.de>
2012-04-07 23:54:00 +0200

    CactusVersion added. Central place for appname, copyright and version data.

---
+++ CactusVersion.py
@@ -0,0 +1,10 @@
+
+
+appname ="Cactus OPML-Editor"
+appnameshort = "Cactus"
+
+copyright = 'Copyright 2011-2012 Karsten Wolf'
+
+version = "0.2.7"
+
+user_agent = "%s/%s +https://github.com/karstenw/Cactus-opml-Outliner" % (appname, version)
