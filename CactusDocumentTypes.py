865db15251dc704ab5a40e116c770d4035fe62c8
Karsten Wolf <karstenwo@web.de>
2012-06-11 12:58:31 +0200

    Initial. The items in this file represent more of a roadmap. OPML is nearly OK, RSS can be read but not written and currently I'm thinking that Cactus needs a native document type.

---
+++ CactusDocumentTypes.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+
+CactusOPMLType = u'Cactus Outline'
+# CactusOPMLFileExtensions = [ 'opml', 'xml']
+CactusOPMLFileExtensions = [ 'opml' ]
+CactusOPMLOSTypes = [ 'OPML', '****' ]
+
+
+CactusRSSType = u'Cactus RSS'
+# CactusRSSFileExtensions = [ 'rss', 'xml']
+CactusRSSFileExtensions = [ 'rss' ]
+CactusRSSOSTypes = [ '****' ]
+
+
+CactusTEXTType = u'Cactus Text'
+CactusTEXTFileExtensions = [ 'txt', ]
+CactusTEXTOSTypes = [ 'TEXT', 'utxt' ]
+
+
+CactusSQLITEType = u'Cactus Sqlite'
+CactusSQLITEFileExtensions = [ 'sqlite', ]
+CactusSQLITEOSTypes = [ '****' ]
+
+
+CactusXOXOType = u'Cactus XOXO'
+CactusXOXOFileExtensions = [ 'xoxo', 'xml', 'html']
+CactusXOXOOSTypes = [ '****' ]
+
+
+CactusXMLType = u'XML File'
+CactusXMLFileExtensions = [ 'xml', ]
+CactusXMLOSTypes = [ '****' ]
+
+
+CactusEMACSORGType = u'Cactus Emacs ORG'
+CactusEMACSORGFileExtensions = [ 'org', ]
+CactusEMACSORGOSTypes = [  ]
+
+
+CactusTABLEType = u'Cactus Table'
+CactusTABLEFileExtensions = [ 'table', ]
+CactusTABLEOSTypes = [ '****' ]
+
