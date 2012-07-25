e80a0d9292e51848458a7a0feee78c34aacdca2a
Karsten Wolf <karstenwo@web.de>
2012-07-25 14:26:12 +0200

    comments

--- CactusDocumentTypes.py
+++ CactusDocumentTypes.py
@@ -1,43 +1,52 @@
 # -*- coding: utf-8 -*-
 
+# the different document types
 
+# works
 CactusOPMLType = u'Cactus Outline'
 # CactusOPMLFileExtensions = [ 'opml', 'xml']
 CactusOPMLFileExtensions = [ 'opml' ]
 CactusOPMLOSTypes = [ 'OPML', '****' ]
 
 
+# can currently only be opened when linked from an opml file
 CactusRSSType = u'Cactus RSS'
 # CactusRSSFileExtensions = [ 'rss', 'xml']
 CactusRSSFileExtensions = [ 'rss' ]
 CactusRSSOSTypes = [ '****' ]
 
 
+# don't know yet, if this is useful
 CactusTEXTType = u'Cactus Text'
 CactusTEXTFileExtensions = [ 'txt', ]
 CactusTEXTOSTypes = [ 'TEXT', 'utxt' ]
 
 
+# a want have
 CactusSQLITEType = u'Cactus Sqlite'
 CactusSQLITEFileExtensions = [ 'sqlite', ]
 CactusSQLITEOSTypes = [ '****' ]
 
 
+# a want have
 CactusXOXOType = u'Cactus XOXO'
 CactusXOXOFileExtensions = [ 'xoxo', 'xml', 'html']
 CactusXOXOOSTypes = [ '****' ]
 
 
+# a want have; an outliner as a general xml editor
 CactusXMLType = u'XML File'
 CactusXMLFileExtensions = [ 'xml', ]
 CactusXMLOSTypes = [ '****' ]
 
 
+# haven't looked into it yet
 CactusEMACSORGType = u'Cactus Emacs ORG'
 CactusEMACSORGFileExtensions = [ 'org', ]
 CactusEMACSORGOSTypes = [  ]
 
 
+# this is on the delete list
 CactusTABLEType = u'Cactus Table'
 CactusTABLEFileExtensions = [ 'table', ]
 CactusTABLEOSTypes = [ '****' ]
