0c6f98168400bb820f27cdbea1149dc9256c145d
Karsten Wolf <karstenwo@web.de>
2013-07-15 21:38:30 +0200

    version 0.5.1

--- CactusVersion.py
+++ CactusVersion.py
@@ -6,7 +6,7 @@ appnameshort = "Cactus"
 
 copyright = 'Copyright 2011-2013 Karsten Wolf'
 
-version = "0.5.0"
+version = "0.5.1"
 
 # 
 user_agent = "%s/%s +https://github.com/karstenw/Cactus-opml-Outliner" % (appname, version)
