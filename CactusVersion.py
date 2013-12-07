e978f4473af0e5e811df471ccccbe443ceeda933
Karsten Wolf <karstenwo@web.de>
2013-12-07 14:31:18 +0100

    v0.6.1 bug fix and new preference: new document on start

--- CactusVersion.py
+++ CactusVersion.py
@@ -6,7 +6,7 @@ appnameshort = "Cactus"
 
 copyright = 'Copyright 2011-2013 Karsten Wolf'
 
-version = "0.6.0"
+version = "0.6.1"
 
 # 
 user_agent = "%s/%s +https://github.com/karstenw/Cactus-opml-Outliner" % (appname, version)
