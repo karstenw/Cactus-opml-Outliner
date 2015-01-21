0ff46f1d728182c208fc50a19a43ae937df2b6fe
Karsten Wolf <karstenwo@web.de>
2015-01-21 17:30:24 +0100

    Initial.

---
+++ CactusURL.py
@@ -0,0 +1,58 @@
+
+# -*- coding: utf-8 -*-
+
+
+"""A class to handle all of Cactus URL conversion needs.
+"""
+
+import sys
+import os
+
+import traceback
+
+import datetime
+import unicodedata
+
+import struct
+
+import mactypes
+import appscript
+asc = appscript
+
+import CactusVersion
+kwdbg = CactusVersion.developmentversion
+kwlog = CactusVersion.developmentversion
+
+import pdb
+
+import re
+
+import urllib
+import urllib2
+import urlparse
+
+import Foundation
+NSURL = Foundation.NSURL
+NSString = Foundation.NSString
+
+
+def NSURL2str( nsurl ):
+    if isinstance(nsurl, NSURL):
+        return str(nsurl.absoluteString())
+    return nsurl
+
+
+class CactusURL(object):
+    def __init__(self, url):
+        # str, unicode or NSURL
+        self.inputurl = url
+        t = type(url)
+        if t in (str, unicode):
+            self.url = url
+        elif t in (NSURL,):
+            self.url = NSURL2str( url )
+        p = urlparse.urlparse( self.url )
+        self.schema = p.schema
+    def ascachepath(self):
+        pass
+
